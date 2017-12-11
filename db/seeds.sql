a45f39dfc5819582526b91875f0526e2aabac052
Dawn Suggs <dawnsuggs@Dawns-Air.att.net>
2017-12-11 01:20:17 -0800

    still debugging

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Chili Burger', true, 171012);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Pastrami Burger', false, 171012);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Cheddar', true, 171012);
